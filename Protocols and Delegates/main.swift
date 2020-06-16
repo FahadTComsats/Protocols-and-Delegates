5422570782ad2a50a24da6c9795e558eb2220819
Fahad Tariq <47610084+FahadTComsats@users.noreply.github.com>
2020-06-16 14:32:02 +0500

    Initial Commit

---
+++ Protocols and Delegates/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Protocols and Delegates
+//
+//  Created by Fahad Tariq on 16/06/2020.
+//  Copyright © 2020 Fahad Tariq. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
