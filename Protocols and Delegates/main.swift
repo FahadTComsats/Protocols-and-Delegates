46799b3a7e0c2039d71ec6f4a2fd5a3ace7b2541
Fahad Tariq <47610084+FahadTComsats@users.noreply.github.com>
2020-06-16 16:17:39 +0500

    Completed

--- Protocols and Delegates/main.swift
+++ Protocols and Delegates/main.swift
@@ -6,7 +6,59 @@
 //  Copyright © 2020 Fahad Tariq. All rights reserved.
 //
 
-import Foundation
+// I'm here to explain the concept of Protocols and Delegates
+//Following is the example of the everyday scenario of hospital
 
-print("Hello, World!")
+protocol AdvanceLifeSupport {
+    func performCPR()
+}
+
+class EmergencyCallHendler {
+    
+    var delegate: AdvanceLifeSupport?
+    
+    func assessSituation(){
+        print("Yo! What happened")
+    }
+    func medicalEmergency(){
+        delegate?.performCPR()
+    }
+    
+}
+
+struct Paramedic: AdvanceLifeSupport {
+    
+    init(handler: EmergencyCallHendler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        
+        print("The Paramedic boy saved the life of human being by performing chest compression")
+    }
+}
+
+
+class Doctor: AdvanceLifeSupport{
+    
+    init(hander: EmergencyCallHendler) {
+        hander.delegate = self
+    }
+    
+    func performCPR() {
+        print("Your daddy is here to save your day baby!")
+    }
+    func choking(){
+        print("Choking the patient")
+    }
+    
+    
+    
+}
+
+let shaktiman = EmergencyCallHendler()
+let superman = Paramedic(handler: shaktiman)
+
+shaktiman.assessSituation()
+shaktiman.medicalEmergency()
 
